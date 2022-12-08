e9893f5f8aededa3011f4f7e3b36bd037097614d
minbong <minbong@DESKTOP-PQT0H3Q>
2022-12-08 11:31:40 +0900

    test1 커밋

---
+++ test/src/test/test1.java
@@ -0,0 +1,29 @@
+package test;
+
+public class test1 {
+
+	String apple;
+	String banana;
+	String kiwi;
+	
+	public String getApple() {
+		return apple;
+	}
+	public void setApple(String apple) {
+		this.apple = apple;
+	}
+	public String getBanana() {
+		return banana;
+	}
+	public void setBanana(String banana) {
+		this.banana = banana;
+	}
+	public String getKiwi() {
+		return kiwi;
+	}
+	public void setKiwi(String kiwi) {
+		this.kiwi = kiwi;
+	}
+	
+	
+}
